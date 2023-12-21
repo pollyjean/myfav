08ad5935f5b70bd3a0db786b462cb9dbf4817d63
sky(skysurfr) <skysurfr@me.com>
2023-12-21 22:41:10 +0900

    :package: Add firebase package

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZ-Bm7AroQOccK_87cOa24pEHz2KscQbg",
+  authDomain: "myfav-6ea29.firebaseapp.com",
+  projectId: "myfav-6ea29",
+  storageBucket: "myfav-6ea29.appspot.com",
+  messagingSenderId: "302881329918",
+  appId: "1:302881329918:web:b3995f93677d0654adcbaf",
+  measurementId: "G-K0ZCDBEHTP",
+};
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
