0198ac200f86ba6ce4b6fa7e70b026e027471088
sky(skysurfr) <skysurfr@me.com>
2024-01-06 00:04:48 +0900

    :art: Limited timeline stream

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,8 +1,15 @@
-import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { db } from "../firebase";
 import Fav from "./Fav";
+import { Unsubscribe } from "firebase/database";
 
 export interface FavInterface {
   id: string;
@@ -15,28 +22,34 @@ export interface FavInterface {
 
 const Timeline = () => {
   const [favs, setFavs] = useState<FavInterface[]>([]);
-  const fetchFavs = async () => {
-    const favsQuery = query(
-      collection(db, "favs"),
-      orderBy("createdAt", "desc")
-    );
-    await onSnapshot(favsQuery, (snapshot) => {
-      const stream = snapshot.docs.map((doc) => {
-        const { fav, userId, username, createdAt, photo } = doc.data();
-        return {
-          id: doc.id,
-          fav,
-          userId,
-          username,
-          createdAt,
-          photo,
-        };
-      });
-      setFavs(stream);
-    });
-  };
+
   useEffect(() => {
+    let unsubscribe: Unsubscribe | null = null;
+    const fetchFavs = async () => {
+      const favsQuery = query(
+        collection(db, "favs"),
+        orderBy("createdAt", "desc"),
+        limit(20)
+      );
+      unsubscribe = await onSnapshot(favsQuery, (snapshot) => {
+        const stream = snapshot.docs.map((doc) => {
+          const { fav, userId, username, createdAt, photo } = doc.data();
+          return {
+            id: doc.id,
+            fav,
+            userId,
+            username,
+            createdAt,
+            photo,
+          };
+        });
+        setFavs(stream);
+      });
+    };
     fetchFavs();
+    return () => {
+      unsubscribe && unsubscribe();
+    };
   }, []);
   return (
     <Wrapper>
